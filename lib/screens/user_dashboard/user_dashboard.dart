6f96b0cbcaa6dab0784b65fca094e8363ae522f3
Rohit Daftari <rohitdaftari3@gmail.com>
2022-12-23 13:25:34 +0530

    Fix Ui

--- lib/screens/user_dashboard/user_dashboard.dart
+++ lib/screens/user_dashboard/user_dashboard.dart
@@ -18,48 +18,53 @@ class _UserDashboardState extends State<UserDashboard> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: DefaultTabController(
-          length: 3,
-          initialIndex: 0,
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                TabBar(
-                  unselectedLabelColor: Colors.grey,
-                  labelColor: Colors.white,
-                  labelPadding: EdgeInsets.zero,
-                  indicator: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Color(0xFF0C331F)),
-                  tabs: [
-                    Tab(
-                      text: "Daily profit/loss",
-                    ),
-                    Tab(
-                      text: "Weekly earnings",
-                    ),
-                    Tab(
-                      text: "Running Monthly total Roz",
-                    ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dashboard"),
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: DefaultTabController(
+        length: 3,
+        initialIndex: 0,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              TabBar(
+                isScrollable: true,
+                unselectedLabelColor: Colors.grey,
+                labelColor: Colors.white,
+                indicator: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Color(0xFF0C331F)),
+                tabs: [
+                  Tab(
+                    text: "Daily profit/loss",
+                  ),
+                  Tab(
+                    text: "Weekly earnings",
+                  ),
+                  Tab(
+                    text: "Running Monthly total Roz",
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    DailyProfitLoss(),
+                    WeeklyEarning(),
+                    RunningMonthlyTotalRoz(),
                   ],
                 ),
-                SizedBox(
-                  height: 20,
-                ),
-                Expanded(
-                  child: TabBarView(
-                    children: [
-                      DailyProfitLoss(),
-                      WeeklyEarning(),
-                      RunningMonthlyTotalRoz(),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
